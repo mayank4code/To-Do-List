1f62ae71e9d1caeefdf31fe82f542cdfac763975
mayank4code <119710987+mayank4code@users.noreply.github.com>
2023-01-29 22:33:24 +0530

    intial commit

---
+++ app.js
@@ -0,0 +1,86 @@
+// jshint version:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+// Ejs Included
+app.set('view engine', 'ejs') ;
+// body-parser Included
+app.use(bodyParser.urlencoded({extended:true}));
+
+var items = ["Buy Food" , "Cook Food" , "Eat Food"];
+
+app.get("/" , (req , resp )=>{
+    var today = new Date ();
+    var options ={
+        weekday: "long",
+        day:"numeric",
+        month:"long"
+    };
+
+    var day = today.toLocaleDateString("en-US" , options );
+    
+    resp.render("list" , {
+        kindOfDay : day ,
+        newListItems: items
+    });
+});
+
+
+app.post("/" , (req ,res)=>{
+    var item = req.body.newItem ;
+    items.push(item);
+
+    res.redirect("/");
+})
+
+app.listen(3000,()=>{
+    console.log("Server Started on Port 3000")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+// var currentDay = today.getDay();
+    
+//     var day = "";
+//     // var arrdays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday" ,"Saturday" ];
+
+//     switch (currentDay) {
+//         case 0:
+//             day= "Sunday";
+//             break;
+//         case 1:
+//             day= "Monday";
+//             break;
+//         case 2:
+//             day= "Tuesday";
+//             break;
+//         case 3:
+//             day= "Wednesday";
+//             break;
+//         case 4:
+//             day= "Thursday";
+//             break;
+//         case 5:
+//             day= "Friday";
+//             break;
+//         case 6:
+//             day= "Saturday";
+//             break;
+//         default:
+//             day = "something went Wrong!";
+//             console.log("Error! Current Day is equal to: " + currentDay);
+//             break;
+//     }
+
